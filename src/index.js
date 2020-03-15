c12c0791f734b5544be9e6d70975436f2ee94c11
SthefanneBatista <estudanteb73@gmail.com>
2020-03-15 00:43:28 -0300

    crud using react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootswatch/dist/cerulean/bootstrap.min.css'
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
